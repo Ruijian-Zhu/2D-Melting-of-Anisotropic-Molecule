f2a332f0c1800862e076dd2149bfc88de163903c
Ruijian-Zhu <168736133+Ruijian-Zhu@users.noreply.github.com>
2024-05-03 22:13:11 +0800

    Update paircorre.cpp

--- translation/paircorre.cpp
+++ translation/paircorre.cpp
@@ -1,6 +1,7 @@
 // C++ code for the calculation of translational function
 // Note that the crystal axis orientation has to be determined at first
 // Written by Ruijian Zhu (ITP-CAS) in Jan 2023
+// 2496 molecules, 2000 snapshots, 1 CPU, requires about 30 min
 
 #include<iostream>
 #include<cstdio>
